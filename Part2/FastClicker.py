6eadc8e44838b027c6d29a0e6e79ad4403809cd3
Ali Messaoudi <83890004+MESSDEV@users.noreply.github.com>
2023-05-18 16:10:14 +0100

    Update FastClicker.py

--- Part2/FastClicker.py
+++ Part2/FastClicker.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 pygame.init()
-'''creating the program window''
+'''creating the program window'''
 back = (200, 255, 255) ##background color
 mw = pygame.display.set_mode((500, 500)) #main window
 mw.fill(back)
